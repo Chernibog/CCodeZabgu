602eb0e4bf1c88f6d6ae38ad20a1f4c501c9a14b
Chernibog <nikitanofamily@gmail.com>
2025-09-25 22:53:03 +0900

    Create matrix_utils.h

---
+++ 692g/matrix_utils.h
@@ -0,0 +1,8 @@
+// matrix_utils.h
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+
+void fill_matrix(int **matrix, int n);
+int max_in_trian_matrix(int **matrix, int n);
+
+#endif // MATRIX_UTILS_H
