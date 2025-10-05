933cd6fb2530346b85d9ff4d04f40398dbfa791e
Chernibog <nikitanofamily@gmail.com>
2025-10-05 18:18:27 +0900

    Update student.h

--- 507b/student.h
+++ 507b/student.h
@@ -2,25 +2,17 @@
 #ifndef STUDENT_H
 #define STUDENT_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-// Максимальное количество учеников
 #define MAX_STUDENTS 1000
-// Максимальная длина строки
-#define MAX_LINE 256
+#define MAX_NAME 50
 
-// Структура ученика
 typedef struct {
-    char surname[100];  // Фамилия
-    char name[100];     // Имя
-    int grade;          // Год обучения
-    char letter;        // Буква класса
+    char name[MAX_NAME];
+    char surname[MAX_NAME];
+    int grade;      // номер класса (например, 10)
+    char letter;    // буква класса (например, 'А')
 } Student;
 
-// Прототипы функций
 int load_students(const char *filename, Student students[]);
 void find_classmates_in_parallel_classes(const Student students[], int count);
 
-#endif // STUDENT_H
+#endif
