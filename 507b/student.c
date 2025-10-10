fc6371860111f1482f31f24c2b476717b009eb4a
Chernibog <nikitanofamily@gmail.com>
2025-10-10 21:45:56 +0900

    Update student.c

--- 507b/student.c
+++ 507b/student.c
@@ -1,47 +1,37 @@
-#include "student.h"
-#include <stdio.h>
-#include <string.h>
-
 void find_classmates_in_parallel_classes_no_array(const char* filename) {
     FILE* file1 = fopen(filename, "r");
+    if (!file1) {
+        printf("Не удалось открыть файл %s\n", filename);
+        return;
+    }
 
     char name1[MAX_NAME], surname1[MAX_NAME];
     int grade1;
     char letter1;
     int found = 0;
-
-    // читаем первого ученика
-    //%[^,] - чтение фамилии до запятой
+    //чтение из файла1 данных
     while (fscanf(file1, "%s %[^,],%d%c", name1, surname1, &grade1, &letter1) == 4) {
         FILE* file2 = fopen(filename, "r");
-        if (!file2) {
-            fclose(file1);
-            return;
-        }
-
         char name2[MAX_NAME], surname2[MAX_NAME];
         int grade2;
         char letter2;
 
-        // Пропускаем учеников до текущей позиции
-        int skip = 0;
-        while (skip == 0 &&
-               fscanf(file2, "%s %[^,],%d%c", name2, surname2, &grade2, &letter2) == 4) {
+        // прокручивание файла2 до текущего ученика из файла1
+        while (fscanf(file2, "%s %[^,],%d%c", name2, surname2, &grade2, &letter2) == 4) {
+            //strcmp - сравнение строк
             if (strcmp(name2, name1) == 0 &&
                 strcmp(surname2, surname1) == 0 &&
                 grade2 == grade1 &&
                 letter2 == letter1) {
-                skip = 1; // нашли начало "после текущего"
-                break;
+                break; // Нашли — останавливаемся, следующие записи будут "после"
             }
         }
 
-        // читаем всех последующих ученикрв
-        //%[^,] - чтение фамилии до запятой
+        // теперь сравниваем со теми, кто идёт после текущго ученика
         while (fscanf(file2, "%s %[^,],%d%c", name2, surname2, &grade2, &letter2) == 4) {
-            if (strcmp(surname1, surname2) == 0 &&
-                grade1 == grade2 &&
-                letter1 != letter2) {
+            if (strcmp(surname1, surname2) == 0 &&   // одинаковая фамилия
+                grade1 == grade2 &&                  // одинаковый класс (цифра)
+                letter1 != letter2) {                // разные буквы → параллельные
 
                 if (!found) {
                     printf("Однофамильцы в параллельных классах:\n");
@@ -52,6 +42,7 @@ void find_classmates_in_parallel_classes_no_array(const char* filename) {
                        name2, surname2, grade2, letter2);
             }
         }
+
         fclose(file2);
     }
 
